3196fb0f2e41a2a42eedaf72b23121ca31c15f13
rob1694 <robertb1694@gmail.com>
2021-03-19 15:01:30 -0400

    finally got itgit add .

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const sentence = [];
+
+function writeCards(nameArray, occasion) {
+    for (let i = 0; i < nameArray.length; i++) {
+        sentence[i] = `Thank you, ${nameArray[i]}, for the wonderful ${occasion} gift!`;
+        }
+        return sentence;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown > -1) {
+      console.log(countDown--);
+    }
+}
